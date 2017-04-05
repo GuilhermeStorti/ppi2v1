778d8842ae7da8694d277b522c916162b5c864ce
guilherme.storti <Gvale@2017>
2017-04-05 18:33:35 -0300

    Primeiro Commit

---
+++ src/main/java/com/unitri/ppi/V1ppiApplication.java
@@ -0,0 +1,12 @@
+package com.unitri.ppi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V1ppiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V1ppiApplication.class, args);
+	}
+}
